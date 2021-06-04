7c71beed047129f177d5435b3cc0b49a4798a46c
Jordy Manner <jordy.manner@milkcreation.fr>
2021-06-04 09:33:36 +0000

    field >> 1.0.x-dev

--- resources/views/checkbox/index.php
+++ resources/views/checkbox/index.php
@@ -3,11 +3,10 @@
  * @var Pollen\Field\FieldViewLoaderInterface $this
  */
 ?>
-<?php $this->before(); ?>
+<?php $this->layout($this->get('theme') . "-wrapper", $this->all()); ?>
 <?php echo $this->partial('tag', [
     'tag'   => 'input',
     'attrs' => $this->get('attrs', []),
 ]);
 ?>
 <?php echo $this->get('label'); ?>
-<?php $this->after();
---
+++ resources/views/checkbox/toggle-wrapper.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var Pollen\Field\FieldViewLoaderInterface $this
+ */
+?>
+<?php $this->before(); ?>
+    <div class="FieldCheckbox-toggleWrapper">
+        <?php echo $this->section('content'); ?>
+    </div>
+<?php $this->after();
